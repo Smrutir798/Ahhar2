f92e68ffe62e81a43f1ec6ceecfb70e748f00f64
SMRUTI RANJAN SWAIN <114172631+Smrutir798@users.noreply.github.com>
2026-06-12 16:51:17 +0530

    Delete frontend directory

--- frontend/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-  ownerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  description: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Category', categorySchema);
